af2d45215481c2f6ff5c7f04bc8f39e328dd95c4
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-08-18 11:37:44 -0400

    Create Nosexd.py

---
+++ Nosexd.py
@@ -0,0 +1,19 @@
+import turtle 
+
+turtle.bgcolor('black')
+
+turtle.color("black")     
+
+turtle.pensize(3) 
+
+turtle.speed(180)
+
+
+for colores in ("blue", "blue4", "firebrick4", "firebrick", "BlueViolet", "DarkOrchid4", "AliceBlue"):
+
+   for x in range(30):
+    turtle.speed(180)
+    turtle.color(colores)
+    turtle.circle(100)
+    turtle.left(10)
+    turtle.hideturtle()
